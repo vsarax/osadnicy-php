41c31cc5bc24318e697ddddc3f81a15e47ca79e7
unknown <saraszewczykk@wp.pl>
2023-02-24 11:08:08 +0100

    Registration form validation, css stylesheet and recaptcha added

--- rejestracja.php
+++ rejestracja.php
@@ -1,21 +1,64 @@
 <?php
     session_start();
 
-    if(isset($_POST['submit']))
-    {
-        $url='https://www.google.com/recaptcha/api/siteverify';
-        $secret='6Le7k6QkAAAAAPkCXirmOz-j71u8vhzf70zjo-WR';
-        $response=$_POST['token_generate'];
-        $remoteip=$_SERVER['REMOTE_ADDR'];
+    if(isset($_POST['email'])) {
+        $success=true;
+        $login=$_POST['login'];
 
-        $request=file_get_contents($url.'?secret'.$secret.'response='.$response);
-        $result=json_decode($request);
+        if((strlen($login)<3) || (strlen($login)>20)) {
+            $success=false;
+            $_SESSION['e_login']="Login musi posiadać od 3 do 20 znaków";
+        }
 
-        if($response->success){
-                /* Kontynuj działanie skryptu */
-            } else {
-                die("Nieprawidłowa odpowiedź CAPTCHA");
-            }
+        if(ctype_alnum($login)==false) {
+            $success=false;
+            $_SESSION['e_login']="Login może składać się tylko z liter i cyfr (bez polskich znaków)";
+        }
+
+        $email=$_POST['email'];
+        $safeemail=filter_var($email, FILTER_SANITIZE_EMAIL);
+
+        if ((filter_var($safeemail, FILTER_VALIDATE_EMAIL)==false) || ($email!=$safeemail)) {
+            $success=false;
+            $_SESSION['e_email']="Podaj poprawny adres email";
+        } 
+    
+        $password1=$_POST['password1'];
+        $password2=$_POST['password2'];
+
+        if((strlen($password1)<8) || (strlen($password1)>20)) {
+            $success=false;
+            $_SESSION['e_password']="Haslo musi posiadać od 8 do 20 znaków";
+        }
+
+        if ($password1!=$password2) {
+            $success=false;
+            $_SESSION['e_password']="Hasła muszą być takie same";
+        }
+
+        $password_hash=password_hash($password1, PASSWORD_DEFAULT);
+
+        if(!isset($_POST['rules']))
+        {
+            $success=false;
+            $_SESSION['e_rules']="Potwierdź akceptację regulaminu";
+        }
+
+        $secret="6LdxDKkkAAAAANrChceVWUqTM6pnYrKy1efucGk5";
+        $check=file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $answer=json_decode($check);
+
+        if($answer->success==false)
+        {
+            $success=false;
+            $_SESSION['e_bot']="Potwierdź, że nie jesteś botem";
+        }
+
+        if($success==true)
+        {
+            echo "Udana walidacja";
+            exit();
+        }
     }
 ?>
 
@@ -26,27 +69,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Osadnicy - załóż darmowe konto</title>
-    <script src="https://www.google.com/recaptcha/api.js?render=6Le7k6QkAAAAAIcWGgBOdifqW7rZnEVGy0Aeef1Z"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://www.google.com/recaptcha/api.js"></script>
 </head>
 <body>
     <form method="post">
-    Nick: <br><input type="text" name="nick"><br>
+        Login: <br><input type="text" name="login"><br>
+        <?php
+            if(isset($_SESSION['e_login'])) {
+                echo '<div class="error">'.$_SESSION['e_login'].'</div>';
+                unset($_SESSION['e_login']);
+            }
+        ?>
         E-mail: <br><input type="text" name="email"><br>
+        <?php
+            if(isset($_SESSION['e_email'])) {
+                echo '<div class="error">'.$_SESSION['e_email'].'</div>';
+                unset($_SESSION['e_email']);
+            }
+        ?>
         Hasło: <br><input type="password" name="password1"><br>
+        <?php
+            if(isset($_SESSION['e_password'])) {
+                echo '<div class="error">'.$_SESSION['e_password'].'</div>';
+                unset($_SESSION['e_password']);
+            }
+        ?>
         Powtórz hasło: <br><input type="password" name="password2"><br><br>
         <label>
-            <input type="checkbox" name="rules"> Akceptuję regulamin<br><br>
+           <input type="checkbox" name="rules"> Akceptuję regulamin<br>
         </label>
-        <input type="hidden" name="token_generate" id="token_generate">
-        <input type="submit" value="Załóż konto"/>
-    </form>
+        <?php
+            if(isset($_SESSION['e_rules'])) {
+                echo '<div class="error">'.$_SESSION['e_rules'].'</div>';
+                unset($_SESSION['e_rules']);
+            }
+        ?>
+        <div class="g-recaptcha" data-sitekey="6LdxDKkkAAAAAIlnWb5lzFopt1O9G18DvhMXM68L"></div>
+        <?php
+            if(isset($_SESSION['e_bot'])) {
+                echo '<div class="error">'.$_SESSION['e_bot'].'</div>';
+                unset($_SESSION['e_bot']);
+            }
+        ?>
+        <input type="submit" value="Załóż konto">
+    </form> 
+
 </body>
 </html>
-<script>
-        grecaptcha.ready(function() {
-          grecaptcha.execute('6Le7k6QkAAAAAIcWGgBOdifqW7rZnEVGy0Aeef1Z', {action: 'submit'}).then(function(token) {
-              var response=document.getElementById("token_generate)");
-              response.value=token;
-          });
-        });
-  </script>
--- zaloguj.php
+++ zaloguj.php
@@ -21,38 +21,45 @@
         $password=$_POST['password'];
         
         $login = htmlentities($login, ENT_QUOTES, "UTF-8");
-        $haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
 
-        if ($result=@$connection->query(sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
-        mysqli_real_escape_string($connection,$login),
-        mysqli_real_escape_string($connection,$password))))
+        if ($result=@$connection->query(sprintf("SELECT * FROM uzytkownicy WHERE user='%s'",
+        mysqli_real_escape_string($connection, $login))))
         {
             $users_num=$result->num_rows;
             if($users_num>0)
             {
-                $_SESSION['loggedin']=true;
-
                 $row=$result->fetch_assoc();
-                $_SESSION['id']=$row['id'];
-                $_SESSION['user']=$row['user'];
-                $_SESSION['drewno']=$row['drewno'];
-                $_SESSION['kamien']=$row['kamien'];
-                $_SESSION['zboze']=$row['zboze'];
-                $_SESSION['email']=$row['email'];
-                $_SESSION['dnipremium']=$row['dnipremium'];
 
-                unset($_SESSION['error']);
-                $result->free_result();
-                header('Location: gra.php');
+                if(password_verify($password, $row['pass'])) 
+                {
+                    $_SESSION['loggedin']=true;
+                    $_SESSION['id']=$row['id'];
+                    $_SESSION['user']=$row['user'];
+                    $_SESSION['drewno']=$row['drewno'];
+                    $_SESSION['kamien']=$row['kamien'];
+                    $_SESSION['zboze']=$row['zboze'];
+                    $_SESSION['email']=$row['email'];
+                    $_SESSION['dnipremium']=$row['dnipremium'];
 
-            } 
-            else {
-                $SESSION['error']='<span style="color:red">
+                    unset($_SESSION['error']);
+                    $result->free_result();
+                    header('Location: gra.php');
+                }
+                else 
+                {
+                    $_SESSION['error']='<span style="color:red">
+                    Nieprawidłowy login lub haslo</span>';
+                    header('Location: index.php');
+                } 
+            
+             } 
+             else {
+                $_SESSION['error']='<span style="color:red">
                 Nieprawidłowy login lub haslo</span>';
                 header('Location: index.php');
-            }
-        }
+                }
 
+        }
         $connection->close();
     }
 ?>
